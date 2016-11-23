2ee1d2e145672009a5ad7bbe5984d55b83d77f1e
Daniel King <daniel.oliver.king@gmail.com>
2016-11-23 13:29:50 -0800

    Add methods for making http request to each url, unzipping the response data, processing the response body from a string into a hash

--- github_archive_service.rb
+++ github_archive_service.rb
@@ -1,8 +1,6 @@
-require 'open-uri'
+require 'net/http'
 require 'zlib'
 require 'json'
-
-require_relative './url_generator'
 
 module GithubArchiveService
   def GithubArchiveService.create_histogram raw_data, event_name=nil
@@ -15,10 +13,46 @@ module GithubArchiveService
     end
   end
   
-  def GithubArchiveService.get_raw_data url
-    zipped = open(url)
-    unzipped = Zlib::GzipReader.new(zipped).read
+  def GithubArchiveService.get_response_data base_url, urls
+    base_uri_obj = URI(base_url)
+    Net::HTTP.start(base_uri_obj.host, base_uri_obj.port) do |http|
+      urls.each do |url|
+        uri_obj = URI(url)
+
+        request = Net::HTTP::Get.new uri_obj
+        response = http.request request
+        entries = process_response response
+
+        yield entries
+      end
+    end
+  end
+
+  def GithubArchiveService.process_response response
+    body = StringIO.new response.body
+    unzipped = Zlib::GzipReader.new(body).read
     
-    unzipped.split("\n").map {|entry| JSON.parse entry}
+    unzipped.split("\n").map {|str| JSON.parse str}
+  end
+
+  def GithubArchiveService.get_repo_name event
+    if event.has_key? 'repository'
+      return {
+        owner: event['repository']['owner'],
+        repo_name: event['repository']['name']
+      }
+    elsif event.has_key? 'repo'
+      owner, repo_name = event['repo']['name'].split("/")
+      return {
+        owner: owner,
+        repo_name: repo_name
+      }
+    else
+      nil
+    end
+  end
+
+  def GithubArchiveService.get_histogram_key repo
+    "#{repo[:owner]}/#{repo[:repo_name]}"
   end
 end
