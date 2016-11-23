82cae0e73e5c03e166c45efc9d39aaa67249276f
Daniel King <daniel.oliver.king@gmail.com>
2016-11-23 09:57:25 -0800

    Change UrlGenerator to generate the correct set of urls, with no curly braces in the urls

--- url_generator.rb
+++ url_generator.rb
@@ -1,62 +1,23 @@
 require 'date'
 
 module UrlGenerator
+  
   def UrlGenerator.generate after, before
-    after_date_time = DateTime.parse after
-    before_date_time = DateTime.parse before
+    after_dt = DateTime.parse after
+    before_dt = DateTime.parse before
 
-    duration_in_days = before_date_time.day - after_date_time.day + 1
+    after_dt.step(before_dt, 1/24.0).map do |datetime|
+      year = datetime.year
+      month = to_two_digit datetime.month
+      day = to_two_digit datetime.day
+      hour = datetime.hour
 
-    case duration_in_days
-    when 1
-      generate_single_day_url after_date_time, before_date_time
-    when 2
-      generate_two_day_urls after_date_time, before_date_time
-    else
-      generate_multi_day_urls after_date_time, before_date_time
+      url_string year, month, day, hour
     end
   end
 
   class << self
     private
-
-    def generate_single_day_url after_date_time, before_date_time
-      year = after_date_time.year
-      month = after_date_time.month
-      day = to_two_digit(after_date_time.day)
-      hour = "{#{after_date_time.hour}..#{before_date_time.hour}}"
-
-      [url_string(year, month, day, hour)]
-    end
-
-    def generate_two_day_urls after_date_time, before_date_time
-      after_year = after_date_time.year
-      after_month = after_date_time.month
-      after_day = to_two_digit(after_date_time.day)
-      after_hour = "{#{after_date_time.hour}..23}"
-
-      before_year = before_date_time.year
-      before_month = before_date_time.month
-      before_day = to_two_digit(before_date_time.day)
-      before_hour = "{0..#{before_date_time.hour}}"
-
-      [
-        url_string(after_year, after_month, after_day, after_hour),
-        url_string(before_year, before_month, before_day, before_hour)
-      ]
-    end
-
-    def generate_multi_day_urls after_date_time, before_date_time
-      result = []
-      after_date_time.next_day.upto before_date_time.prev_day do |date|
-        year, month, day = date.year, date.month, to_two_digit(date.day)
-        result.push url_string(year, month, day, "{0..23}")
-      end
-
-      first, last = generate_two_day_urls after_date_time, before_date_time
-
-      result.unshift(first).push(last)
-    end
 
     def url_string year, month, day, hour
       "http://data.githubarchive.org/#{year}-#{month}-#{day}-#{hour}.json.gz"
