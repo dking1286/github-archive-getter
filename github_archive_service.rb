9fb9519cae77fa96e1848461763ccca142e49d0d
Daniel King <daniel.oliver.king@gmail.com>
2016-11-24 20:57:29 -0800

    Move private helper methods into anonymous class

--- github_archive_service.rb
+++ github_archive_service.rb
@@ -5,6 +5,10 @@ require 'json'
 module GithubArchiveService
   def GithubArchiveService.get_each_response base_url, urls
     base_uri_obj = URI(base_url)
+
+    # Net::HTTP.start keeps the connection open if the server allows it
+    # when making multiple http requests
+    # We should do this for performance reasons
     Net::HTTP.start(base_uri_obj.host, base_uri_obj.port) do |http|
       urls.each do |url|
         uri_obj = URI(url)
@@ -16,13 +20,6 @@ module GithubArchiveService
         yield entries
       end
     end
-  end
-
-  def GithubArchiveService.process_response response
-    body = StringIO.new response.body
-    unzipped = Zlib::GzipReader.new(body).read
-    
-    unzipped.split("\n").map {|str| JSON.parse str}
   end
 
   def GithubArchiveService.get_repo_name event
@@ -44,5 +41,17 @@ module GithubArchiveService
 
   def GithubArchiveService.get_histogram_key repo
     "#{repo[:owner]}/#{repo[:repo_name]}"
+  end
+
+  # Anonymous class to hold private helper methods
+  class << self
+    private
+
+    def process_response response
+      body = StringIO.new response.body
+      unzipped = Zlib::GzipReader.new(body).read
+      
+      unzipped.split("\n").map {|str| JSON.parse str}
+    end
   end
 end
